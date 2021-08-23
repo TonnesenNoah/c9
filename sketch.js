431c88a50cbd9a33ca4346b59c804b6ebf397086
TonnesenNoah <88952739+TonnesenNoah@users.noreply.github.com>
2021-08-22 17:51:00 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+
+  function setup() {
+  
+  createCanvas(400,400);
+  btn_red = createButton("RED");
+  btn_red.position(100,50);
+  btn_red.mousePressed(red_bg)
+
+  btn_green = createButton("GREEN");
+  btn_green.position(250,50);
+  btn_green.mousePressed(green_bg)
+  }
+
+  funtion red_bg()
+  {
+  r = 255;
+  g = 0;
+  b = 0;
+  }
+
+  funtion green_bg()
+  {
+  r = 0;
+  g = 255;
+  b = 0;
+  }
+
+  function draw() 
+  {
+  background(30);
+
+  }
+
+
+
+
